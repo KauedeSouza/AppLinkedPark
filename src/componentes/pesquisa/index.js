24bb297d72f31773685df73b48ac60d5389ab532
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-11-07 11:06:45 -0300

    pesquisa

--- src/componentes/pesquisa/index.js
+++ src/componentes/pesquisa/index.js
@@ -1,12 +1,101 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { View, TextInput, FlatList, Text, Image, TouchableOpacity } from 'react-native';
 
-const Pesquisa = ({ data, navigation }) => {
+const data = [
+  {
+    idLazer: '1',
+    nome: 'Atividade 1',
+    descricao: 'Descrição da Atividade 1',
+    imagem: 'URL_DA_IMAGEM_1'
+  },
+  {
+    idLazer: '2',
+    nome: 'Atividade 2',
+    descricao: 'Descrição da Atividade 2',
+    imagem: 'URL_DA_IMAGEM_2'
+  },
+  // Adicione mais itens conforme necessário
+];
 
+const Pesquisa = ({ navigation }) => {
+  const [query, setQuery] = useState('');
 
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      key={item.idLazer}
+      style={{ height: 160 }}
+      onPress={() => navigation.navigate('TelaDetalhes', item)}
+    >
+      <View
+        style={{
+          backgroundColor: '#B1D3C1',
+          marginTop: 15,
+          borderRadius: 35,
+          width: 340,
+          height: 150,
+          marginLeft: 9,
+        }}
+      >
+        <Image source={{ uri: item.imagem }} style={styles.Imagens} />
+        <Text
+          style={{
+            marginLeft: 140,
+            marginTop: -145,
+            fontSize: 18,
+          }}
+        >
+          {item.nome}
+        </Text>
+
+        <Text
+          style={{
+            marginLeft: 140,
+            marginTop: 10,
+            fontSize: 10,
+          }}
+        >
+          {item.descricao}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+
+  const filteredData = data.filter((item) =>
+    item.nome.toLowerCase().includes(query.toLowerCase())
+  );
+
+  return (
+    <View style={{ flex: 1, padding: 16 }}>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+          marginBottom: 16,
+          paddingHorizontal: 8,
+        }}
+        placeholder="Digite sua pesquisa"
+        onChangeText={(text) => setQuery(text)}
+        value={query}
+      />
+      <FlatList
+        data={filteredData}
+        keyExtractor={(item) => item.idLazer}
+        renderItem={renderItem}
+      />
+    </View>
+  );
 };
 
+const styles = {
+  Imagens: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    position: 'absolute',
+    left: 20,
+    top: 25,
+  },
+};
 
 export default Pesquisa;
-
